b2c9b208428624c3d4c8b64d0d0d2b9f189fb129
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2021-01-09 17:08:54 +0200

    add host and port

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import app from './app';
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
+const HOST = '0.0.0.0';
 
 async function init() {
-  app.listen(4000, () => {
-    console.log(`app is running on port ${PORT}`);
+  app.listen(4000, HOST, () => {
+    console.log(`app is running on port ${PORT}, host ${HOST}`);
   });
 }
 
