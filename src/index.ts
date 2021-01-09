167c4b79fcf318b4ee81824b4a16568b4ade1e1f
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2021-01-09 17:25:08 +0200

    trying this fix for port issue

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import app from './app';
 
 const PORT = process.env.PORT || 4000;
-const HOST = '0.0.0.0';
 
 async function init() {
-  app.listen(4000, HOST, () => {
-    console.log(`app is running on port ${PORT}, host ${HOST}`);
+  app.listen(PORT as number, () => {
+    console.log(`app is running on port ${PORT}`);
   });
 }
 
